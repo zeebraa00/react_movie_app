f3cdeebb7a4dd678935dcfd7effa456ffd69adcf
zeebraa00 <zeebraa00@gmail.com>
2020-07-15 16:20:14 +0900

    [edit] 클래스형 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,67 +1,52 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I Like {name}</h2>
-      <h4>{rating}</h4>
-      <img src={picture} alt={name} />
-    </div>
-  )
-}
+// App 클래스는 React.Component를 상속받았다. 즉, React.Component 클래스의 기능을 추가한 App 클래스
+// 클래스형 컴포넌트를 사용하는 이유 : state를 사용하기 위해서
+class App extends React.Component {
 
-const foodLike=[
-  {
-    id : 1,
-    name : "Kimchi",
-    image : "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating : 5
-  },
-  {
-    id : 2,
-    name : "Samgyeopsal",
-    image : "https://lh3.googleusercontent.com/proxy/JceN4UW98jDmUcenGS3PpAMZgHNcEe3nA8ID37lVjT9If4UdhLipFN_QcFPrFk7D_O8npCLCpFXY05auryH1oeu-GzBgKL9P4ryFxT_Bl0qvow8ms_pZuuGOmgwPDWfXeQnV07ZJPw",
-    rating : 4.9
-  },
-  {
-    id : 3,
-    name : "Bibimbap",
-    image : "https://img.etoday.co.kr/pto_db/2019/07/600/20190726153503_1350707_1200_876.jpg",
-    rating : 3
-  },
-  {
-    id : 4,
-    name : "Doncasu",
-    image : "https://mp-seoul-image-production-s3.mangoplate.com/951194_1545618824282984.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
-    rating : 3.9
-  },
-  {
-    id : 5,
-    name : "Kimbab",
-    image : "https://recipe1.ezmember.co.kr/cache/recipe/2016/06/29/e7401296033ab8e4297cd53d71e1bba91.jpg",
-    rating : 4.8
+  constructor(props) {
+    super(props);
+    console.log('hello');
   }
-];
 
-// 화살표 함수 : function renderFood(dish) 랑 같은 뜻임
+  state = {
+    count : 0,
+  };
 
+  add = () => {
+    this.setState(current => ({ // current에는 현재 state가 넘어온다
+      count : current.count + 1
+    }))
+  }
 
-function App() {
-  return (
-    <div>
-      {/* Food component에, foodLike의 원소 dish마다, dish.name과 dish.image 두 개를 props로 넘겨줘라 */}
-      {foodLike.map(dish =>
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-      )}
-    </div>
-  );
-}
+  minus = () => {
+    this.setState(current => ({
+      count : current.count - 1
+    }))
+  }
 
-Food.propTypes = {
-  name : PropTypes.string.isRequired,
-  picture : PropTypes.string.isRequired,
-  rating : PropTypes.number.isRequired
+  componentDidMount() {
+    console.log('component rendered')
+  }
+
+  componentDidUpdate() {
+    console.log('i just updated')
+  }
+
+  componentWillUnmount() {
+    console.log('goodbye')
+  }
+
+  render() {
+    console.log('rendering')
+    return (
+      <div>
+        <h1> The number is {this.state.count} </h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
