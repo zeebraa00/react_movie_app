9504be72c5267b9364f8a8dcaaa3edf32435cdbe
zeebraa00 <zeebraa00@gmail.com>
2020-07-17 20:06:32 +0900

    [edit] 로그 지웠음

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './About.css'
 
 function About(props) {
-    console.log(props)
+    // console.log(props)
     return (
         <div className="about__container">
             <span>
-                "내 이름은 정재헌 react를 배웠지"
+                "내 이름은 정재헌, react를 배웠지"
             </span>
             <span>
                 - https://github.com/zeebraa00/react_movie_app
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -34,7 +34,7 @@ class Home extends React.Component {
           <div className="movies">
             {
               movies.map((movie) => {
-                console.log(movie);
+                // console.log(movie);
                 return (
                   <Movie
                     key={movie.id}
