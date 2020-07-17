0e7dd4a762450bd63756f18ee66e29706ea264fa
zeebraa00 <zeebraa00@gmail.com>
2020-07-17 19:01:15 +0900

    [edit] about page

---
+++ build/precache-manifest.1c0db0058917a3f8a33a66fe3a33f077.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9bd3a0c787daa3f994bc5cf14e2b80",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "f31ff9cfbf7682b17bdd",
+    "url": "/react_movie_app/static/css/main.4ffed7a6.chunk.css"
+  },
+  {
+    "revision": "33dc50890de65e9ab059",
+    "url": "/react_movie_app/static/js/2.026418d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_movie_app/static/js/2.026418d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f31ff9cfbf7682b17bdd",
+    "url": "/react_movie_app/static/js/main.d76e7ecd.chunk.js"
+  },
+  {
+    "revision": "b2eab6cde90deae0fa53",
+    "url": "/react_movie_app/static/js/runtime-main.e749ff8d.js"
+  }
+]);
--- src/routes/About.js
+++ src/routes/About.js
@@ -6,10 +6,10 @@ function About(props) {
     return (
         <div className="about__container">
             <span>
-                "Freedom is the freedom to say that two plus two make four. If that is granted, all else follows."
+                "내 이름은 정재헌 react를 배웠지"
             </span>
             <span>
-                - George Orwell, 1984
+                - https://github.com/zeebraa00/react_movie_app
             </span>
         </div>
     )
